d8d9c11cb376e3be768acd5c0817b7fefad50098
wlingxiao <wlingxiao@outlook.com>
2017-04-03 13:48:53 +0800

    完善目标管理添加按钮

--- app/src/main/java/me/yuanye/taglife/TargetManagerActivity.java
+++ app/src/main/java/me/yuanye/taglife/TargetManagerActivity.java
@@ -2,12 +2,25 @@ package me.yuanye.taglife;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class TargetManagerActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.target_manager_activity);
+        toolbar = (Toolbar)findViewById(R.id.target_manager_toolbar);
+        toolbar.setTitle("目标");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_target_manager, menu);
+        return true;
     }
 }
